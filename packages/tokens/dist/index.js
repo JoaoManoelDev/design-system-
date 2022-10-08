458bde2f1404cdea6943edc81e553e348f2f78ef
João <joaomanoeldev@gmail.com>
2022-10-08 11:52:02 -0300

    Design System in React - Configuring React Package

--- packages/tokens/dist/index.js
+++ packages/tokens/dist/index.js
@@ -17,14 +17,20 @@ var __copyProps = (to, from, except, desc) => {
 };
 var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
 
-// src/index.tsx
+// src/index.ts
 var src_exports = {};
 __export(src_exports, {
-  App: () => App
+  colors: () => colors,
+  fontSizes: () => fontSizes,
+  fontWeights: () => fontWeights,
+  fonts: () => fonts,
+  lineHeights: () => lineHeights,
+  radii: () => radii,
+  space: () => space
 });
 module.exports = __toCommonJS(src_exports);
 
-// ../tokens/dist/index.mjs
+// src/colors.ts
 var colors = {
   white: "#FFF",
   black: "#000",
@@ -42,15 +48,79 @@ var colors = {
   ignite900: "#00291D"
 };
 
-// src/index.tsx
-var import_jsx_runtime = require("react/jsx-runtime");
-function App() {
-  return /* @__PURE__ */ (0, import_jsx_runtime.jsx)("h1", {
-    style: { color: colors.ignite300 },
-    children: "Hello World"
-  });
-}
+// src/space.ts
+var space = {
+  1: "0.25rem",
+  2: "0.5rem",
+  3: "0.75rem",
+  4: "1rem",
+  5: "1.25rem",
+  6: "1.5rem",
+  7: "1.75rem",
+  8: "2rem",
+  10: "2.5rem",
+  12: "3rem",
+  16: "4rem",
+  20: "5rem",
+  40: "10rem",
+  64: "16rem",
+  80: "20rem"
+};
+
+// src/radii.ts
+var radii = {
+  px: "1px",
+  xs: "4px",
+  sm: "6px",
+  md: "8px",
+  lg: "16px",
+  full: "99999px"
+};
+
+// src/fonts.ts
+var fonts = {
+  default: "Roboto, sans-serif",
+  code: "monospace"
+};
+
+// src/font-sizes.ts
+var fontSizes = {
+  xxs: "0.625rem",
+  xs: "0.75rem",
+  sm: "0.875rem",
+  md: "1rem",
+  lg: "1.125rem",
+  xl: "1.25rem",
+  "2xl": "1.5rem",
+  "4xl": "2rem",
+  "5xl": "2.25rem",
+  "6xl": "3rem",
+  "7xl": "4rem",
+  "8xl": "4.5rem",
+  "9xl": "6rem"
+};
+
+// src/font-weights.ts
+var fontWeights = {
+  regular: "400",
+  medium: "500",
+  bold: "700"
+};
+
+// src/line-heights.ts
+var lineHeights = {
+  shorter: "125%",
+  short: "140%",
+  base: "160%",
+  tall: "180%"
+};
 // Annotate the CommonJS export names for ESM import in node:
 0 && (module.exports = {
-  App
+  colors,
+  fontSizes,
+  fontWeights,
+  fonts,
+  lineHeights,
+  radii,
+  space
 });
